836e5b818cdf8936f4079ce66d6f3bb05aaae380
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-16 18:54:40 +0530

    SI: Added props rendering for each book and learned props concept

--- basic-projects/tutorial-course/tutorial/src/index.js
+++ basic-projects/tutorial-course/tutorial/src/index.js
@@ -3,27 +3,35 @@ import ReactDom from "react-dom";
 
 import "./index.css";
 
+const firstBook = {
+  imgSrc: "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg",
+  title: "IKIGAI",
+  author: "Héctor García and Francesc Miralles",
+};
+
+const secondBook = {
+  imgSrc: "https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY218_.jpg",
+  title: "Think Like A Monk",
+  author: "Jay Shetty",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book book={firstBook} />
+      <Book book={secondBook} />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "IKIGAI";
-  const author = "Héctor García and Francesc Miralles";
+const Book = (props) => {
   return (
     <article className="book">
-      <h1>{title}</h1>
-      <img
-        src="https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg"
-        alt="book-cover"
-      />
+      <h1>{props.book.title}</h1>
+      <img src={props.book.imgSrc} alt="book-cover" />
       <section>
         <small>by</small>
-        <h3>{author}</h3>
+        <h3>{props.book.author}</h3>
       </section>
     </article>
   );
