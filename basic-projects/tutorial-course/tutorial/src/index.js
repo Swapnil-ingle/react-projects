5fb8dd4bdb813c2ee4fd581e75d885c416f58cf5
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-16 17:41:20 +0530

    SI: Added a basic book-display application in React

--- basic-projects/tutorial-course/tutorial/src/index.js
+++ basic-projects/tutorial-course/tutorial/src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const Person = () => <h2>Swapnil Ingle</h2>;
-const Desc = () => <p>This is person desc!</p>;
-const PersonSubHeading = () => <h3>Software Dev</h3>;
+import "./index.css";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <PersonSubHeading />
-      <Desc />
-    </div>
+    <section className="booklist">
+      <Book />
+    </section>
   );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  const title = "IKIGAI";
+  const author = "Héctor García and Francesc Miralles";
+  return (
+    <article className="book">
+      <h1>{title}</h1>
+      <img
+        src="https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg"
+        alt="book-cover"
+      />
+      <section>
+        <small>by</small>
+        <h3>{author}</h3>
+      </section>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
