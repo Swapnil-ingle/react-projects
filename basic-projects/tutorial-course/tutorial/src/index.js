abc9d4c489169497c81251c15757866d81e81d32
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-16 14:49:43 +0530

    SI: Formatted by 'Prettier' extension

--- basic-projects/tutorial-course/tutorial/src/index.js
+++ basic-projects/tutorial-course/tutorial/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 function Greeting() {
   return <h4>This is my first React component!</h4>;
 }
 
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+ReactDom.render(<Greeting />, document.getElementById("root"));
