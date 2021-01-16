f71af75307ee1b19b5785994833a0569ff7c3824
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-16 16:45:45 +0530

    SI: Updated index.js and completed a basic component of React

--- basic-projects/tutorial-course/tutorial/src/index.js
+++ basic-projects/tutorial-course/tutorial/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const Person = () => <h2>Swapnil Ingle</h2>;
+const Desc = () => <p>This is person desc!</p>;
+const PersonSubHeading = () => <h3>Software Dev</h3>;
+
 function Greeting() {
-  return <h4>This is my first React component!</h4>;
+  return (
+    <div>
+      <Person />
+      <PersonSubHeading />
+      <Desc />
+    </div>
+  );
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
