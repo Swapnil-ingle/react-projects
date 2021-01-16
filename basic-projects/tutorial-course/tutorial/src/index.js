f8514479f6d10ed590ed5a593fc67e9de5edd5bd
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-16 19:45:14 +0530

    SI: Rendering a list and properlist in component

--- basic-projects/tutorial-course/tutorial/src/index.js
+++ basic-projects/tutorial-course/tutorial/src/index.js
@@ -3,23 +3,33 @@ import ReactDom from "react-dom";
 
 import "./index.css";
 
-const firstBook = {
-  imgSrc: "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg",
-  title: "IKIGAI",
-  author: "Héctor García and Francesc Miralles",
-};
-
-const secondBook = {
-  imgSrc: "https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY218_.jpg",
-  title: "Think Like A Monk",
-  author: "Jay Shetty",
-};
+const books = [
+  {
+    id: 1,
+    imgSrc: "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg",
+    title: "IKIGAI",
+    author: "Héctor García and Francesc Miralles",
+  },
+  {
+    id: 2,
+    imgSrc: "https://m.media-amazon.com/images/I/81s6DUyQCZL._AC_UY218_.jpg",
+    title: "Think Like A Monk",
+    author: "Jay Shetty",
+  },
+  {
+    id: 3,
+    imgSrc: "https://m.media-amazon.com/images/I/81cpDaCJJCL._AC_UY218_.jpg",
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book book={firstBook} />
-      <Book book={secondBook} />
+      {books.map((book) => {
+        return <Book key={book.id} book={book} />;
+      })}
     </section>
   );
 }
@@ -28,8 +38,8 @@ const Book = (props) => {
   const book = props.book;
   return (
     <article className="book">
-      <h1>{book.title}</h1>
       <img src={book.imgSrc} alt="book-cover" />
+      <h1>{book.title}</h1>
       <section>
         <small>by</small>
         <h3>{book.author}</h3>
