4046b2e2416eb443d6df1d48c6f1093359ffd820
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-18 23:13:34 +0530

    SI: Added useState with Array example

--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/1-useState/setup/3-useState-array.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const removeThis = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <React.Fragment>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeThis(id)}>
+              Remove This
+            </button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Remove All
+      </button>
+      {/* <button className="btn" onClick={}></button>
+      <button className="btn" onClick={}></button> */}
+    </React.Fragment>
+  );
 };
 
 export default UseStateArray;
