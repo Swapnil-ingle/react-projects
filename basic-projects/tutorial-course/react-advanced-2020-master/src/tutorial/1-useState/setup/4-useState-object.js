5a2dae2422b13da6b19b734b1875a4ac6715207e
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-18 23:27:47 +0530

    SI: Added useState hook with an Object

--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    msg: "This is peter",
+  });
+  console.log(person);
+
+  const changeMsg = () => {
+    setPerson({ ...person, msg: "Hello World!" });
+  };
+
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.msg}</h3>
+      <button className="btn" onClick={changeMsg}>
+        Change Message
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
