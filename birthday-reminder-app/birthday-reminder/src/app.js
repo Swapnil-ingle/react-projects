c3222b92cb2695438600d4327b254eeb0406a540
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-20 07:01:21 +0530

    SI: Completed the birthday-reminder app

--- birthday-reminder-app/birthday-reminder/src/app.js
+++ birthday-reminder-app/birthday-reminder/src/app.js
@@ -5,11 +5,31 @@ import birthdayList from "./data/birthday-list";
 const App = () => {
   const [birthdays, setBirthdays] = React.useState(birthdayList);
 
+  const clearAll = () => {
+    setBirthdays([]);
+  };
+
   return (
     <main>
       <h2>{birthdays.length} Birthdays Today</h2>
-      <div className="btday-list-container"></div>
-      <button className="btn">Clear All</button>
+      <div className="btday-list-container">
+        {birthdays.map((birthday) => {
+          const { picURL, age, name } = birthday;
+
+          return (
+            <article className="birthday">
+              <img src={picURL} alt="" />
+              <section>
+                <h3>{name}</h3>
+                <p>{age} Years Old</p>
+              </section>
+            </article>
+          );
+        })}
+      </div>
+      <button className="btn clr-all-btn" onClick={clearAll}>
+        Clear All
+      </button>
     </main>
   );
 };
--- birthday-reminder-app/birthday-reminder/src/data/birthday-list.js
+++ birthday-reminder-app/birthday-reminder/src/data/birthday-list.js
@@ -1,8 +1,17 @@
 const birthdays = [
   {
-    picURL: "",
+    id: 1,
+    picURL:
+      "https://avatars3.githubusercontent.com/u/30175580?s=460&u=f4f6bac4dc415616cf9b514a6c0a42a749150c6c&v=4",
     age: 24,
     name: "Swapnil Ingle",
+  },
+  {
+    id: 2,
+    picURL:
+      "https://scontent.fpnq16-1.fna.fbcdn.net/v/t1.0-9/91578519_2654827021293226_2319433423904571392_o.jpg?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_ohc=TQZTO7-XzKkAX9WtYEI&_nc_ht=scontent.fpnq16-1.fna&oh=bfcada395b741bf9e3ea1937abb64658&oe=602CEB12",
+    age: 23,
+    name: "Vaishnavi Joshi",
   },
 ];
 
