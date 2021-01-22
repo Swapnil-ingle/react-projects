b9fff8278769d55a5ebd65e7039c7a129334f0d8
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-22 06:48:22 +0530

    SI: Added mutliple-returns based on conditions

--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,53 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarsons";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default User");
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        }
+
+        setIsLoading(false);
+        setIsError(true);
+        throw new Error(resp.statusText);
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>;
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
