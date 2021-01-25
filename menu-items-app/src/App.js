27f30ec7652dff4a2bd3bcdfcfcdb079651222b1
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-25 20:01:15 +0530

    SI: Started with Menu item app

--- menu-items-app/src/App.js
+++ menu-items-app/src/App.js
@@ -16,31 +16,14 @@ function App() {
         <article className="menu-item">
           <section className="menu-item-img">
             <img
+              className="photo"
               src="https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fGZvb2R8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
               alt=""
             />
           </section>
           <div className="item-info">
             <header>
-              <h4>Food Item 1</h4>
-              <h4 className="price">$10</h4>
-            </header>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Possimus, aliquam.
-            </p>
-          </div>
-        </article>
-        <article className="menu-item">
-          <section className="menu-item-img">
-            <img
-              src="https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fGZvb2R8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt=""
-            />
-          </section>
-          <div className="item-info">
-            <header>
-              <h4>Food Item 1</h4>
+              <h3>Food Item 1</h3>
               <h4 className="price">$10</h4>
             </header>
             <p>
