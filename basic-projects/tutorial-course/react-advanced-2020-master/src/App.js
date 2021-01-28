b7873001e379689a144bab1d9cb23c2d8c727c0f
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-01-28 20:56:46 +0530

    SI: Taking data from input form and displaying list for each submit entry. Also added data validation

--- basic-projects/tutorial-course/react-advanced-2020-master/src/App.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 
 function App() {
--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,10 +8,28 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(firstName, email);
+
+    if (firstName && email) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName,
+        email,
+      };
+
+      setPeople((people) => {
+        return [...people, person];
+      });
+
+      setFirstName("");
+      setEmail("");
+    } else {
+      alert("Cannot add person, input value(s) are empty!");
+    }
   };
 
   return (
@@ -40,6 +58,15 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">Add Person</button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div key={id} className="item">
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
