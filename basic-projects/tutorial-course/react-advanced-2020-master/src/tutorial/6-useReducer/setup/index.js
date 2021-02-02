d0bb4ce7fb008156445e6bff1e60ca0385c653bc
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-02-02 21:01:12 +0530

    SI: Start working on useReducer React hook:

--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,53 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer, useRef } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
+const reducer = (state, action) => {};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "Hello World!",
+};
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const inputRefContainer = useRef("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = inputRefContainer.current.value;
+    console.log(name);
+    if (name) {
+    } else {
+    }
+  };
+
+  return (
+    <>
+      {state.isModalOpen && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            // value={name}
+            // onChange={(e) => setName(e.target.value)}
+            ref={inputRefContainer}
+          />
+          <button type="submit">Add Person</button>
+        </div>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
