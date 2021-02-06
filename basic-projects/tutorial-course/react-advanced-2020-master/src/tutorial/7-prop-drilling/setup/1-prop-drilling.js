4b3b75664aee8af9fa20b028ce0aed5b44257b97
Vaishnavisj <vaishnavisjoshi29@gmail.com>
2021-02-06 23:03:55 +0530

    SI: Started learning prop-drilling as a pre-req to useContext

--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
+import {data} from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+
+  return <section>
+    <h3>Prop Drilling</h3>
+    <List people={people}/>
+  </section>
+};
+
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person) => {
+        return <SinglePerson key={person.id}/>
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({id, name}) => {
+  return <div className="item">
+    <h4>Single Item</h4>
+  </div>
 };
 
 export default PropDrilling;
