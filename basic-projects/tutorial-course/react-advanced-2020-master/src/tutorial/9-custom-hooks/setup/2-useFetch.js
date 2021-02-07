3342349930321cb72b30466af1f10894c2d3abab
swapnil-ingle <dr.swapnilingle@gmail.com>
2021-02-07 10:44:45 +0530

    SI: Learned custom hooks in React

--- basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ basic-projects/tutorial-course/react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setData(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [url]);
+
+  return { loading, data };
+};
