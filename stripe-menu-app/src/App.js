97ccafa1813b0fa2af2289b6cf2bebb92a0a85c9
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-02-14 21:56:08 +0530

    SI: Started working on Stripe Menu application on ReactJS

---
+++ stripe-menu-app/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2>Stripe Submenu Setup</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ stripe-menu-app/src/Sidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <h2>Sidebar Component</h2>;
+};
+
+export default Sidebar;
