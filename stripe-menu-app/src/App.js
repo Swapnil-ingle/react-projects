bd06b96f0f9f9dddbe6d5a1a236674d728780a63
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-02-14 23:03:45 +0530

    SI: 1. Cleaned up eslintcache file-tracking. 2. Boiler-plate code for stripe-menu-app

--- stripe-menu-app/src/App.js
+++ stripe-menu-app/src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
 function App() {
   return (
     <>
-      <h2>Stripe Submenu Setup</h2>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
     </>
   );
 }
--- stripe-menu-app/src/context.js
+++ stripe-menu-app/src/context.js
@@ -1,2 +1,15 @@
 import React, { useState, useContext } from "react";
 import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  <AppContext.Provider value={{ sublinks }}>{children}</AppContext.Provider>;
+};
+
+// Custom Hook
+export const useGlobalContext = () => {
+  return useContext(contextValue);
+};
+
+export { AppContext, AppProvider };
