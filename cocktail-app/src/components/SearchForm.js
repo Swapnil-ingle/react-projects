5bb4df520d4f4d0d575b086ce5e81e15675ce958
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-02-21 19:07:17 +0530

    SI: Made some progress on the cocktail-app

--- cocktail-app/src/components/SearchForm.js
+++ cocktail-app/src/components/SearchForm.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
--- cocktail-app/src/pages/About.js
+++ cocktail-app/src/pages/About.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quo dicta
+        voluptates officiis nostrum cupiditate quia, omnis sequi vitae similique
+        obcaecati dolore rem in ipsam repellendus rerum explicabo tempora
+        sapiente!
+      </p>
+    </section>
   );
 };
 
