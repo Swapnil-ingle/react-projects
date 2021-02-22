f72b044085226a2dbda0f230a0737924e28a6b49
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-02-22 22:52:28 +0530

    SI: Concluded the cocktail-app project - Learned the application of react-router and global-context.

--- cocktail-app/src/context.js
+++ cocktail-app/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     try {
       const response = await fetch(`${url}${searchTerm}`);
       const data = await response.json();
@@ -43,11 +43,11 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  };
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchDrinks();
-  }, [searchTerm]);
+  }, [searchTerm, fetchDrinks]);
 
   return (
     <AppContext.Provider
