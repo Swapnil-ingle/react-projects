0b412a10b4c3b9fd0baadf314a5e69abb4e60784
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-03-04 08:16:39 +0530

    SI: Stylized undo button

--- habit-tracking-grid-app/src/components/Card/Card.jsx
+++ habit-tracking-grid-app/src/components/Card/Card.jsx
@@ -6,16 +6,16 @@ import { formatDateObj } from "../../utils/utils";
 
 const Card = ({ id, name, startDate, doneTasksOn }) => {
   const { toggleToday } = useGlobalContext();
+  const doneToday = doneTasksOn.indexOf(formatDateObj(new Date())) !== -1;
   return (
     <div className="card">
       <h2>{name}</h2>
       <Grid startDate={startDate} doneTasksOn={doneTasksOn} />
-      <button onClick={() => toggleToday(id)}>
-        {`${
-          doneTasksOn.indexOf(formatDateObj(new Date())) !== -1
-            ? "Undo"
-            : "Mark Today"
-        }`}
+      <button
+        id={`${doneToday ? "undo-btn" : ""}`}
+        onClick={() => toggleToday(id)}
+      >
+        {`${doneToday ? "Undo" : "Mark Today"}`}
       </button>
     </div>
   );
