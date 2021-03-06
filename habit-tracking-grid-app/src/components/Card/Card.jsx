9901c118ef003c37a609c5dc3ea45c85c32c8d9a
Swapnil Ingle <dr.swapnilingle@gmail.com>
2021-03-06 16:32:40 +0530

    SI: Cleaned up files

--- habit-tracking-grid-app/src/components/Card/Card.jsx
+++ habit-tracking-grid-app/src/components/Card/Card.jsx
@@ -6,20 +6,12 @@ import { formatDateObj } from "../../utils/utils";
 import DeleteCardBtn from "./DeleteCardBtn/DeleteCardBtn";
 
 const Card = ({ id, name, description, startDate, doneTasksOn }) => {
-  const { toggleToday, deleteHabit } = useGlobalContext();
+  const { toggleToday } = useGlobalContext();
   const doneToday = doneTasksOn.indexOf(formatDateObj(new Date())) !== -1;
-
-  const handleCardDelete = (e) => {
-    e.preventDefault();
-    deleteHabit(id);
-  };
 
   return (
     <div className="card">
-      {/* <button onClick={(e) => handleCardDelete(e)} id="remove-card"> */}
       <DeleteCardBtn id={id} />
-      {/* <CancelIcon style={{ fontSize: 40 }} /> */}
-      {/* </button> */}
       <div className="card-header">
         <h2>{name}</h2>
         <p>{description}</p>
